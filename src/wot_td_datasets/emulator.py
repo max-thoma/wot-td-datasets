022d3e4425aab4151f0524e44c0b8237f90129ac
max-thoma <70842226+max-thoma@users.noreply.github.com>
2026-04-14 17:59:05 +0200

    :sparkles: Improve emulation quality and experience

--- src/wot_td_datasets/emulator.py
+++ src/wot_td_datasets/emulator.py
@@ -1,15 +1,31 @@
+"""
+Emulate the IoT devices and publish the message log
+"""
+
 import asyncio
 import logging
+from itertools import cycle
 
 from amqtt.client import ConnectError, MQTTClient
 
+import wot_td_datasets.td
 from wot_td_datasets.generate import DataSets, td_datasets
 from wot_td_datasets.mock import generate_device_message_log
 
 logger = logging.getLogger("emulator.py")
 
 
-async def emulate_client(thing) -> None:
+async def emulate_client(thing, sleep) -> None:
+    """
+    Emulate the IoT device and publish the message log.
+
+    Each device can generate a message log by calling `generate_device_message_log`.
+    This function returns an a `DeviceMessageLog` object. A `DeviceMessageLog`
+    holds a list of MessageLogList. Each MessageLogList is a list of MessageLog;
+    i.e., logs: List[List[MessageLog]]. Hence, each affordance has a list of message logs.
+    In one iteration, each affordance publishes one message from the log, then it sleeps for `sleep` seconds.
+
+    """
     client_id = thing.td().id
     client = MQTTClient(client_id=client_id)
     try:
@@ -18,39 +34,51 @@ async def emulate_client(thing) -> None:
         logger.error(con)
         return
 
-    publish_task = []
     logs = generate_device_message_log(thing=thing)
-    for log in logs.logs:
-        for log_ in log.logs:
-            topic_ = log_.topic
-            payload_ = log_.payload.encode("utf-8")
-            retain_ = log_.retain
-            publish_task.append(
-                asyncio.ensure_future(
-                    client.publish(topic=topic_, message=payload_, retain=retain_)
+    log_cycle = [cycle(logs.logs) for logs in logs.logs]
+    log_pool = cycle(log_cycle)
+
+    try:
+        while True:
+            publish_task = []
+            for _ in range(len(log_cycle)):
+                cycle_iter = next(log_pool)
+                next_log = next(cycle_iter)
+                publish_task.append(
+                    asyncio.ensure_future(
+                        client.publish(
+                            topic=next_log.topic,
+                            message=next_log.payload.encode("utf-8"),
+                            retain=next_log.retain,
+                        )
+                    )
                 )
-            )
-    await asyncio.wait(publish_task)
+            await asyncio.wait(publish_task)
+            await asyncio.sleep(delay=sleep)
+    except asyncio.CancelledError:
+        await client.disconnect()
+        raise
 
-    await client.disconnect()
 
-
-async def run_tasks(things):
+async def run_tasks(things, sleep):
 
     tasks = []
     for thing in things:
-        tasks.append(asyncio.ensure_future(emulate_client(thing)))
+        tasks.append(asyncio.ensure_future(emulate_client(thing, sleep)))
 
-    await asyncio.wait(tasks)
+    try:
+        await asyncio.wait(tasks)
+    except asyncio.CancelledError:
+        logger.info("Task cancled sucessfully")
 
 
-def main():
-    formatter = "[%(asctime)s] :: %(levelname)s :: %(name)s :: %(message)s"
+def main(sleep: int = 10):
     formatter = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=formatter)
 
-    asyncio.run(run_tasks(td_datasets(DataSets.CUSTOM)))
+    asyncio.run(run_tasks(td_datasets(DataSets.CUSTOM), sleep))
 
 
 if __name__ == "__main__":
-    main()
+    wot_td_datasets.td.MESSAGE_NUM = 1
+    main(sleep=10)
